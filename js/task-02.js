b0c618f002bb50273cae1976987abe73e690c087
Kamil Trzmiel <k.trzmiel@gmail.com>
2023-03-16 15:55:02 +0100

    task 02

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,11 @@
-const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
-];
+const ingredients = ['Potatoes', 'Mushrooms', 'Garlic', 'Tomatos', 'Herbs', 'Condiments'];
+
+const ingredientsEl = document.querySelector('#ingredients');
+console.log(ingredientsEl);
+
+ingredients.forEach(ingredient => {
+  const listItem = document.createElement('li');
+  listItem.textContent = ingredient;
+  listItem.classList.add('item');
+  ingredientsEl.append(listItem);
+});
