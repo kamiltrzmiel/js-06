b433230a599bf733c8d18a427e6771a384f26ef6
Kamil Trzmiel <k.trzmiel@gmail.com>
2023-03-23 08:53:51 +0100

    update task 08

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,19 @@
+const formEl = document.querySelector('.login-form');
+const emailEl = document.querySelector('#email');
+const passEl = document.querySelector('#password');
 
+formEl.addEventListener('submit', handleSubmit);
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
+
+  if (email.value === '' || password.value === '') {
+    alert('All fields should be filled in!');
+  } else {
+    console.log(`Email: ${email.value}, Password: ${password.value}`);
+    event.currentTarget.reset();
+  }
+}
