1577a7d9b114b048e7e126916d0eb3beb1940bb8
Kamil Trzmiel <k.trzmiel@gmail.com>
2023-03-23 09:14:36 +0100

    update task 09

--- js/task-09.js
+++ js/task-09.js
@@ -1,5 +1,14 @@
+const btnEl = document.querySelector('.change-color');
+const textEl = document.querySelector('.color');
+
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215)
+  return `#${Math.floor(Math.random() * 16737255)
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+btnEl.addEventListener('click', () => {
+  const rdmColor = getRandomHexColor();
+  document.body.style.backgroundColor = rdmColor;
+  textEl.textContent = rdmColor;
+});
