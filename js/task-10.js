b8e12a300b63786c6a4e7f789522eb43512a9727
Kamil Trzmiel <k.trzmiel@gmail.com>
2023-03-24 19:02:28 +0100

    update task 10

--- js/task-10.js
+++ js/task-10.js
@@ -1,5 +1,30 @@
+const boxesEl = document.querySelector('#boxes');
+const inputBoxEl = document.querySelector('input[type="number"]');
+const createBtnEl = document.querySelector('button[data-create]');
+const destroyBtnEl = document.querySelector('button[data-destroy]');
+
+//COLOR
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+//CREATE
+createBtnEl.addEventListener('click', () => createBoxes(inputBoxEl.value));
+const createBoxes = amount => {
+  for (let i = 0; i < amount; i++) {
+    const box = document.createElement('div');
+    box.style.height = 30 + i * 10 + 'px';
+    box.style.width = 30 + i * 10 + 'px';
+    box.style.margin = '20px';
+    box.style.background = getRandomHexColor();
+    boxesEl.append(box);
+  }
+};
+
+//DESTROY
+destroyBtnEl.addEventListener('click', () => {
+  const destroyBoxes = () => (boxesEl.innerHTML = '');
+  destroyBoxes();
+});
